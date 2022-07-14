62ec4d02ea734558871ca704e2586ecd358e47d2
yhupaul <yhupaul@hotmail.com>
2022-07-14 11:04:08 -0700

    to undertstand rest api, start buiding new todoapp

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.listen(8080, function(){
+  console.log('listening on 8080')
+});
+
+// when someone visits /pet, let's show guidelind about per
+
+app.get('/pet', function(req, res){
+  res.send('This is the page you can shop for your pet');
+});
+
+app.get('/beauty', function(req, res){
+  res.send('This is the page you can shop for your beauty');
+});
